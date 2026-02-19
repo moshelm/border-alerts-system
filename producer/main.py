e2c3c400ea0228b6f911ed1e177f6c1aedc781f7
Moshe <moshe@example.com>
2026-02-19 09:49:21 +0200

    feat: basic main

---
+++ producer/main.py
@@ -0,0 +1,10 @@
+from connection_redis import get_data_from_file, insert_to_queue, producer 
+from priority_logic import sorting_alerts
+
+def main():
+    data = get_data_from_file()
+    sorting_alerts(data)
+    insert_to_queue(data, producer)
+
+if __name__=="__main__":
+    main()
