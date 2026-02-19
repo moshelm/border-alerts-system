9514dceaa03f811886bfb0a199917ff71652e9ad
Moshe <moshe@example.com>
2026-02-19 12:44:25 +0200

    fix: functions and aggrgation

--- analystics-api/dal.py
+++ analystics-api/dal.py
@@ -3,27 +3,27 @@ import mongo_connection as mongo
 coll = mongo.collection
 
 def alerts_border():
-    query = 
-    projection = {"border":1,"priority":1}     
-    res = coll.find(query)
+    query = [{'$group':{"_id":"$border","total":{"$sum":"$priority"}}}]        
+    res = coll.aggregate(query)
     return  res
 def urgent_zones():
-    query = {"$group":{"$_id":"$border","priority":},
-             "$count":{""}}
-    res = coll.find(query)
+    query = [{"$group":{"_id":"$zone","total":{"$sum":{"$priority":'URGENT'}}},
+             },{"$sort":"total"},{"$limit":5}]
+    res = coll.aggregate(query)
     return res
+
 def distance():
-    query = {"$group":{"$_id":"$border","priority":},
-             "$count":{""}}
-    res = coll.find(query)
+    query = [{"$group":{"_id":"$"},
+             "$count":{""}}]
+    res = coll.aggregate(query)
     return res
 def visibility_activity():
-    query = {"$group":{"$_id":"$border","priority":},
-             "$count":{""}}
-    res = coll.find(query)
+    query = [{"$group":{"_id":"$"},
+             "$count":{""}}]
+    res = coll.aggregate(query)
     return res
 def hot_zones():
-    query = {"$group":{"$_id":"$border","priority":},
-             "$count":{""}}
-    res = coll.find(query)
+    query = [{"$group":{"_id":"$"},
+             "$count":{""}}]
+    res = coll.aggregate(query)
     return res
