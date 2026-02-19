9ea8da1b355e5c6b849ffa9bfe54bae7df95cc12
Moshe <moshe@example.com>
2026-02-19 11:34:54 +0200

    fix: file name

--- producer/connection_redis.py
+++ producer/connection_redis.py
@@ -10,16 +10,19 @@ producer = redis.Redis(host=REDIS_HOST, port=REDIS_PORT,db=0,decode_responses=Tr
 
 
 def get_data_from_file():
-    with open("json",'r') as file:
+    with open("data/border_alerts.json",'r') as file:
         data = json.load(file)
     return data
 
 def insert_to_queue(data:list[dict], producer:Redis):
     try:
-        for alert in data:
+        for alert in data:    
+            alert = json.dumps(alert).encode()
+
             if alert['priority'] == 'URGENT':
                 producer.lpush("urgent_queue",alert)
             elif alert['priority'] == 'NORMAL':
-                producer.lpush("normal_queue",alert)
+                producer.lpush("normal_queue:1",alert)
+            
     except Exception as e:
         print(f' failed insertion to queue {e}')
