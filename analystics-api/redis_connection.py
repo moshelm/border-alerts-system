16d51ad292c43771a5de0e54349a22a177760170
Moshe <moshe@example.com>
2026-02-19 12:52:46 +0200

    fix: name

--- analystics-api/redis_connection.py
+++ analystics-api/redis_connection.py
@@ -4,4 +4,4 @@ import os
 REDIS_HOST = os.getenv("REDIS_HOST","localhost")
 REDIS_PORT = os.getenv("REDIS_PORT","6379")
 
-consumer = redis.Redis(host=REDIS_HOST, port=REDIS_PORT,db=0,decode_responses=True)
+manager = redis.Redis(host=REDIS_HOST, port=REDIS_PORT,db=0,decode_responses=True)
