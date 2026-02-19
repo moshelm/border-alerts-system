8b86800b8a0ab16b6f777662595b6f652488ba09
Moshe <moshe@example.com>
2026-02-19 12:52:59 +0200

    feat: cache redis

--- analystics-api/routes.py
+++ analystics-api/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from dal import *
-
+from redis_connection import manager
 router = APIRouter()
 
 def data_to_fastapi(data:list):
@@ -9,27 +9,42 @@ def data_to_fastapi(data:list):
 
 @router.get("/analytics/alerts-by-border-and-priority")
 def alerts_by_border():
-    data = alerts_border
+    data = manager.get("id_result")
+    if data is None:
+        data = alerts_border
+        manager.setex("id_result",10,data)
     data_to_fastapi(data)
     return data
 
 @router.get("/analytics/top-urgent-zones")
 def top_urgent_zones():
-    data =urgent_zones() 
+    data = manager.get("id_result")
+    if data is None:
+        data =urgent_zones() 
+        manager.setex("id_result",10,data)
     data_to_fastapi(data)
     return data
 @router.get("/analytics/distance-distribution")
 def distance_distribution():
-    data = distance() 
+    data = manager.get("id_result")
+    if data is None:
+        data = distance() 
+        manager.setex("id_result",10,data)
     data_to_fastapi(data)
     return data
 @router.get("/analytics/low-visibility-high-activity")
 def low_visibility_high_activity():
-    data = visibility_activity() 
+    data = manager.get("id_result")
+    if data is None:
+        data = visibility_activity() 
+        manager.setex("id_result",10,data)
     data_to_fastapi(data)
     return data
 @router.get("/analytics/hot-zones")
 def get_hot_zones():
-    data = hot_zones() 
+    data = manager.get("id_result")
+    if data is None:
+        data = hot_zones() 
+        manager.setex("id_result",10,data)
     data_to_fastapi(data)
     return data
