cd69986dccb227f0f36a221ddeca87e8fabbe662
Moshe <moshe@example.com>
2026-02-22 20:45:23 +0200

    fix: handle redis cache

--- analystics-api/routes.py
+++ analystics-api/routes.py
@@ -21,37 +21,58 @@ def alerts_by_border():
         data_to_redis = json.dumps(data)
         manager.setex("alerts-by-border-and-priority",10,data_to_redis)
     data_to_fastapi(data)
-    return data
+    return {"status":status,"data":data}
 
 @router.get("/analytics/top-urgent-zones")
 def top_urgent_zones():
     data = manager.get("top-urgent-zones")
-    if data is None:
-        data =urgent_zones() 
-        manager.setex("top-urgent-zones",10,data)
-    data_to_fastapi(data)
-    return data
+    if data: 
+        data = json.loads(data)
+        status = "from redis"
+    elif data is None:
+        data = urgent_zones()
+        status = "from mongo"
+        data_to_redis = json.dumps(data)
+        manager.setex("top-urgent-zones",10,data_to_redis)
+    
+    return {"status":status,"data":data}
+
 @router.get("/analytics/distance-distribution")
 def distance_distribution():
     data = manager.get("distance-distribution")
-    if data is None:
-        data = distance() 
-        manager.setex("distance-distribution",10,data)
-    data_to_fastapi(data)
-    return data
+    if data: 
+        data = json.loads(data)
+        status = "from redis"
+    elif data is None:
+        data = distance()
+        status = "from mongo"
+        data_to_redis = json.dumps(data)
+        manager.setex("distance-distribution",10,data_to_redis)
+    
+    return {"status":status,"data":data}
 @router.get("/analytics/low-visibility-high-activity")
 def low_visibility_high_activity():
     data = manager.get("low-visibility-high-activity")
-    if data is None:
-        data = visibility_activity() 
-        manager.setex("low-visibility-high-activity",10,data)
-    data_to_fastapi(data)
-    return data
+    if data: 
+        data = json.loads(data)
+        status = "from redis"
+    elif data is None:
+        data = visibility_activity()
+        status = "from mongo"
+        data_to_redis = json.dumps(data)
+        manager.setex("low-visibility-high-activity",10,data_to_redis)
+    
+    return {"status":status,"data":data}
 @router.get("/analytics/hot-zones")
 def get_hot_zones():
     data = manager.get("hot-zones")
-    if data is None:
-        data = hot_zones() 
-        manager.setex("hot-zones",10,data)
-    data_to_fastapi(data)
-    return data
+    if data: 
+        data = json.loads(data)
+        status = "from redis"
+    elif data is None:
+        data = hot_zones()
+        status = "from mongo"
+        data_to_redis = json.dumps(data)
+        manager.setex("hot-zones",10,data_to_redis)
+    
+    return {"status":status,"data":data}
