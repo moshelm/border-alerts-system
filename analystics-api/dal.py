93d039a7f714bcef102cb640a2dfc37807171244
Moshe <moshe@example.com>
2026-02-22 11:27:46 +0200

    fix: dal.py alerts_border function

--- analystics-api/dal.py
+++ analystics-api/dal.py
@@ -3,9 +3,23 @@ import mongo_connection as mongo
 coll = mongo.collection
 
 def alerts_border():
-    query = [{'$group':{"_id":"$border","total":{"$sum":"$priority"}}}]        
-    res = coll.aggregate(query)
-    return  res
+    query = [{"$group":{"_id":"$border",
+"total_event":{"$count":{}},
+"urgent_only_count": {
+                "$sum": {
+                    "$cond": [
+                        {"$eq": ["$priority", "URGENT"]}, 
+                        1, 
+                        0]}},
+"normal_only_count": {
+                "$sum": {
+                    "$cond": [
+                        {"$eq": ["$priority", "NORMAL"]}, 
+                        1, 
+                        0]}}}},
+            {"$sort":{"urgent_only_count": -1}}]        
+    result = list(coll.aggregate(query))
+    return  result
 def urgent_zones():
     query = [{"$group":{"_id":"$zone","total":{"$sum":{"$priority":'URGENT'}}},
              },{"$sort":"total"},{"$limit":5}]
